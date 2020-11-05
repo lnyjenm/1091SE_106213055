4f22737331ddcd734b817397b05b8b8ad549599d
陳崚榛 <s106213055@mail1.ncnu.edu.tw>
2020-11-05 14:21:32 +0800

    title update

--- todoAddForm.php
+++ todoAddForm.php
@@ -6,7 +6,7 @@ require("dbconnect.php");
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>無標題文件</title>
+<title>新增代辦事項</title>
 </head>
 <body>
 <h1>Add New Task</h1>
--- todoRpt.php
+++ todoRpt.php
@@ -9,7 +9,7 @@ $result=mysqli_query($conn,$sql) or die("DB Error: Cannot retrieve message.");
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>無標題文件</title>
+<title>已完成工作報表</title>
 </head>
 
 <body>
